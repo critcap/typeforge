4d000e556456ff34a430bce88aa7fb9fa88bb71e
critcap <critcap.dev@gmail.com>
2022-09-22 15:09:35 +0200

    add test started and finished signals, refactor validation process

--- typewars/Core/validator.gd
+++ typewars/Core/validator.gd
@@ -1,31 +1,38 @@
 class_name Validator
 extends Node
 
-signal word_passed
-signal wrong_letter_input
-signal correct_letter_input
+signal started
+signal finished
 
-var _scancodes: Array
+signal word_passed
+signal wrong_letter_input(code)
+signal correct_letter_input(code)
+
+
+var scancodes: PoolIntArray
 var _index: int = 0
 var _errors: int = 0
+var has_started: bool = false
 
 
 func validate(code: int) -> void:
-	if !has_scancodes() || _index >= _scancodes.size():
+	if _index == 0 && !has_started:
+		has_started = true
+		emit_signal("started")
+
+	if scancodes == null || scancodes.empty():
 		return
 
-	if code != _scancodes[_index]:
-		emit_signal("wrong_letter_input")
+	if code != scancodes[_index]:
+		emit_signal("wrong_letter_input", code)
 		_errors += 1
 		print("Error: " + str(_errors))
+		return
 
-	emit_signal("correct_letter_input")
-
-	if _scancodes[_index] == KEY_SPACE:
+	emit_signal("correct_letter_input", code)
+	if scancodes[_index] == KEY_SPACE:
 		emit_signal("word_passed")
-
 	_index += 1
 
-
-func has_scancodes() -> bool:
-	return (_scancodes == null) || _scancodes.empty()
+	if _index >= scancodes.size():
+		emit_signal("finished")
