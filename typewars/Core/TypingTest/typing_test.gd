83a41306f763ecd6c9b5f2cc1e5ce007932e5f0d
critcap <critcap.dev@gmail.com>
2022-09-22 15:07:55 +0200

    add timing modes

--- typewars/Core/TypingTest/typing_test.gd
+++ typewars/Core/TypingTest/typing_test.gd
@@ -3,8 +3,16 @@ extends Node
 
 # TODO add standard tests as constants
 
+# TIME_ATTACK: How many words in 60 seconds
+# RACE: How fast can you write 100 words
+
+
+enum {TIME_ATTACK, RACE}
+
 var content: PoolStringArray setget set_content, get_content
 var scancodes: PoolIntArray
+var mode: int
+
 
 # setters & getters
 func set_content(value) -> void:
