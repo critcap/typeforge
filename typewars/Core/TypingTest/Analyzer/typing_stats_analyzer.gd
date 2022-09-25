76937894c6bf28dc83700a6d270e1563637f4a8d
critcap <critcap.dev@gmail.com>
2022-09-25 21:59:39 +0200

    add wip analyzer class

--- typewars/Core/TypingTest/Analyzer/typing_stats_analyzer.gd
+++ typewars/Core/TypingTest/Analyzer/typing_stats_analyzer.gd
@@ -1,2 +1,45 @@
 class_name TypingStatsAnalyzer
 
+
+static func analyze_typing_stats(typing_stats: Dictionary, time: int, words: int) -> TypingStatsResult:
+	var results = TypingStatsResult.new()
+	results.time = time
+	results.words = words
+
+	for letter in typing_stats.keys():
+		var letter_stats = typing_stats[letter]
+		results.total_errors = letter_stats[ETypingStats.ERRORS]
+		results.total_correct = letter_stats[ETypingStats.ERRORS]
+
+		# get most accurate character
+		if !results.most_accurate_character:
+			results.most_accurate_character = letter
+		else:
+			results.most_accurate_character = compare_accuracy(letter, results.most_accurate_character)
+		# get most inaccurate character
+		if !results.most_inaccurate_character:
+			results.most_inaccurate_character = letter
+		elif letter != results.most_accurate_character:
+			results.most_inaccurate_character = compare_accuracy(letter, results.most_inaccurate_character, true)
+		
+		
+
+			
+	return results
+
+
+static func compare_accuracy(a: Dictionary, b: Dictionary, inverse: bool = false) -> int:
+	var a_accuracy = get_accuracy(a[ETypingStats.CORRECT], a[ETypingStats.TOTAL_COUNT], inverse)
+	var b_accuracy = get_accuracy(b[ETypingStats.CORRECT], b[ETypingStats.TOTAL_COUNT], inverse)
+	if a_accuracy > b_accuracy:
+		return a[ETypingStats.CODE]
+	elif a_accuracy == b_accuracy:
+		var a_count = a[ETypingStats.TOTAL_COUNT]
+		var b_count = b[ETypingStats.TOTAL_COUNT]
+		return a[ETypingStats.CODE] if a_count > b_count else b[ETypingStats.CODE]
+	else:
+		return b[ETypingStats.CODE]
+
+
+static func get_accuracy(correct, total, inverse) -> float:
+	return correct / total if !inverse else (total - correct) / total
