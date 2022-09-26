15699910f29c019d4b3ab698910d64d2b214b341
William <critcap.dev@gmail.com>
2022-09-26 11:18:05 +0200

    add property functions, changed property suffix from character to key

--- typewars/Core/TypingTest/Analyzer/typing_stats_result.gd
+++ typewars/Core/TypingTest/Analyzer/typing_stats_result.gd
@@ -1,24 +1,51 @@
 class_name TypingStatsResult
 extends Reference
 
-var wpm: float
-var cpm: float
-var accuracy: float
-var total_strokes: int
+# Dynamic properties
+var wpm: float setget , get_wpm
+var cpm: float setget , get_cpm
+var accuracy: float setget , get_accuracy
+var total_strokes: int setget , get_total_strokes
 
+# Properties
 var time: int
 var words: int
 var total_errors: int
 var total_correct: int
 var most_pressed_key: int
 var least_pressed_key: int
-var most_accurate_character: int
-var least_accurate_character: int
-var fastest_character: int
-var slowest_character: int
+var fastest_key: int
+var slowest_key: int
+var longest_key: int
+var most_accurate_key: int
+var least_accurate_key: int
 
 var _data: Dictionary
 
 
+# region Setters and Getter#
+func get_wpm() -> float:
+	return words * (60.0 / time) if time != 0 else 0.0
+
+
+func get_cpm() -> float:
+	return total_correct * (60.0 / time) if time != 0 else 0.0
+
+
+func get_accuracy() -> float:
+	return float(total_correct / self.get_total_strokes)
+
+
+func get_total_strokes() -> int:
+	return total_correct + total_errors
+
+
+# endregion
+
+
+# region Public Methods
 func get_character_stats(character: int):
-    return  _data[character]
+	return _data[character]
+
+
+# endregion
