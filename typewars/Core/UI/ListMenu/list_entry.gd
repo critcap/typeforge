58bdc7828909ba37fd77eaca325ee7aa0c2c68e6
critcap <critcap.dev@gmail.com>
2022-09-28 21:39:54 +0200

    refactor list_entry to handle intermediate input from its items and add subentry functionality

--- typewars/Core/UI/ListMenu/list_entry.gd
+++ typewars/Core/UI/ListMenu/list_entry.gd
@@ -1,13 +1,12 @@
 class_name ListEntry
 extends HBoxContainer
 
-onready var indicator: Label = $Indicator
+signal pressed
+
+
+onready var indicator: Label = $IndicatorSelect
 onready var item: Control = $Item
-
-
-func _ready():
-	item.connect("focus_entered", self, "select_item")
-	item.connect("focus_exited", self, "deselect_item")
+onready var subentry: Control = $Subentry
 
 
 func setup(_name: String) -> void:
@@ -25,3 +24,12 @@ func deselect_item():
 	if item.has_focus():
 		item.release_focus()
 	indicator.visible = false
+
+
+func _on_item_pressed():
+	if !Input.is_key_pressed(KEY_SHIFT) || subentry == null || subentry.get_children().empty():
+		emit_signal("pressed")
+		return
+	var children: = subentry.get_children()[0] as Control
+	subentry.visible = true
+	children.grab_focus()
