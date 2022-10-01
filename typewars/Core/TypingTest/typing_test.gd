de23e8eac9cd1bf76f4ba0ab1a46fd02e67134e2
critcap <critcap.dev@gmail.com>
2022-10-01 03:13:53 +0200

    refactor typing_test_mode into seperate enum class

--- typewars/Core/TypingTest/typing_test.gd
+++ typewars/Core/TypingTest/typing_test.gd
@@ -6,9 +6,6 @@ extends Node
 # TIME_ATTACK: How many words in 60 seconds
 # RACE: How fast can you write 100 words
 
-
-enum {TIME_ATTACK, RACE}
-
 var content: PoolStringArray setget set_content, get_content
 var scancodes: PoolIntArray
 var mode: int
