452c8fb21f64db54e888a0a5e277fe995e41d3d7
critcap <critcap.dev@gmail.com>
2022-10-06 22:06:16 +0200

    add formating

--- typewars/Core/Controller/States/init_state.gd
+++ typewars/Core/Controller/States/init_state.gd
@@ -1,80 +1,25 @@
 class_name InitState
 extends TestState
 
-
 const TESTS = {
-	"homerow": {
-	  "content": [
-		"a",
-		"s",
-		"d",
-		"f",
-		"j",
-		"k",
-		"l",
-		";",
-		"g",
-		"h"
-	  ],
-	  "args": {
-		"combine": [
-		  "toprow",
-		  "bottomrow"
-		],
-		"mode": 1,
-		"size": 50
-	  }
+	"homerow":
+	{
+		"content": ["a", "s", "d", "f", "j", "k", "l", ";", "g", "h"],
+		"args": {"combine": ["toprow", "bottomrow"], "mode": 1, "size": 50}
 	},
-	"toprow": {
-	  "content": [
-		"q",
-		"w",
-		"e",
-		"r",
-		"t",
-		"y",
-		"u",
-		"i",
-		"o"
-	  ],
-	  "args": {
-		"combine": [
-		  "homerow",
-		  "bottomrow"
-		],
-		"mode": 1,
-		"size": 50
-	  }
+	"toprow":
+	{
+		"content": ["q", "w", "e", "r", "t", "y", "u", "i", "o"],
+		"args": {"combine": ["homerow", "bottomrow"], "mode": 1, "size": 50}
 	},
-	"bottomrow": {
-	  "content": [
-		"z",
-		"x",
-		"c",
-		"v",
-		"b",
-		"n",
-		"m",
-		",",
-		"."
-	  ],
-	  "args": {
-		"combine": [
-		  "toprow",
-		  "homerow"
-		],
-		"mode": 1,
-		"size": 50
-	  }
+	"bottomrow":
+	{
+		"content": ["z", "x", "c", "v", "b", "n", "m", ",", "."],
+		"args": {"combine": ["toprow", "homerow"], "mode": 1, "size": 50}
 	},
-	"brown_fox": {
-	  "content": "The quick brown fox jumps over the lazy dog",
-	  "args": {
-		"mode": 1,
-		"size": 5
-	  }
-	}
-  }
+	"brown_fox":
+	{"content": "The quick brown fox jumps over the lazy dog", "args": {"mode": 1, "size": 5}}
+}
 
 
 func enter() -> void:
@@ -84,13 +29,12 @@ func enter() -> void:
 
 
 func initialize() -> void:
-
 	owner.validator = Validator.new()
 	owner.stats_collector = TypingStatsCollector.new()
 	owner.add_child(owner.data_loader)
 	owner.add_child(owner.validator)
 	owner.add_child(owner.stats_collector)
-	
+
 	var tests = {}
 	for key in TESTS.keys():
 		var test = TypingTestData.new()
