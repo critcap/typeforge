652906891796723e0c27016206161fa62550e54d
critcap <critcap.dev@gmail.com>
2022-10-07 01:24:13 +0200

    fix typo

--- typewars/ListMenus/StatisticsMenu/statistics_menu.gd
+++ typewars/ListMenus/StatisticsMenu/statistics_menu.gd
@@ -9,9 +9,10 @@ func setup_statistics(result: TypingStatsResult) -> void:
 	title.text = "Typing Statistics"
 	add_child(title)
 	create_entry(str("Total Words:  ", result.words))
-	create_entry(str("Time:  ", result.time, "s"))
+	create_entry(str("Time:  ", result.time, "ms"))
 	create_entry(str("Words Per Minute:  ", result.wpm))
 	create_entry(str("Accuracy:  ", result.accuracy * 100, "%"))
+	create_entry(str("Errors:  ", result.total_errors))
 	add_child(Label.new())
 	var fastest_key = ScancodeConverter.convert_code_to_string(result.fastest_key)
 	var fastest_time = result.get_character_stats(result.fastest_key)[ETypingStats.FASTEST]
@@ -22,9 +23,10 @@ func refresh_statistics(result: TypeRaceTestState) -> void:
 	if get_children().size() < 1:
 		return
 	get_child(0).text = str("Total Words:  ", result.words)
-	get_child(1).text = str("Time:  ", result.time, "s")
+	get_child(1).text = str("Time:  ", result.time, "ms")
 	get_child(2).text = str("Words Per Minute:  ", result.wpm)
 	get_child(3).text = str("Accuracy:  ", result.accuracy * 100, "%")
+	get_child(4).text = str("Errors:  ", result.errors)
 	var fastest_key = ScancodeConverter.convert_code_to_string(result.fastest_key)
 	var fastest_time = result.get_character_stats(result.fastest_key)[ETypingStats.FASTEST]
-	get_child(4).text = str("Fastest Key:  ", fastest_key.to_upper(), " in ", fastest_time, "s")
+	get_child(5).text = str("Fastest Key:  ", fastest_key.to_upper(), " in ", fastest_time, "s")
