84f0a2a78f75894c4525c6d0ce6479457127bfde
critcap <critcap.dev@gmail.com>
2022-10-07 01:30:17 +0200

    fix many typos and nullreferences

--- typewars/ListMenus/StatisticsMenu/statistics_menu.gd
+++ typewars/ListMenus/StatisticsMenu/statistics_menu.gd
@@ -19,14 +19,14 @@ func setup_statistics(result: TypingStatsResult) -> void:
 	create_entry(str("Fastest Key:  ", fastest_key.to_upper(), " in ", fastest_time, "s"))
 
 
-func refresh_statistics(result: TypeRaceTestState) -> void:
+func refresh_statistics(result: TypingStatsResult) -> void:
 	if get_children().size() < 1:
 		return
 	get_child(0).text = str("Total Words:  ", result.words)
-	get_child(1).text = str("Time:  ", result.time, "ms")
-	get_child(2).text = str("Words Per Minute:  ", result.wpm)
-	get_child(3).text = str("Accuracy:  ", result.accuracy * 100, "%")
-	get_child(4).text = str("Errors:  ", result.errors)
+	get_child(1).item.text = str("Time:  ", result.time, "ms")
+	get_child(2).item.text = str("Words Per Minute:  ", result.wpm)
+	get_child(3).item.text = str("Accuracy:  ", result.accuracy * 100, "%")
+	get_child(4).item.text = str("Errors:  ", result.total_errors)
 	var fastest_key = ScancodeConverter.convert_code_to_string(result.fastest_key)
 	var fastest_time = result.get_character_stats(result.fastest_key)[ETypingStats.FASTEST]
-	get_child(5).text = str("Fastest Key:  ", fastest_key.to_upper(), " in ", fastest_time, "s")
+	get_child(5).item.text = str("Fastest Key:  ", fastest_key.to_upper(), " in ", fastest_time, "s")
