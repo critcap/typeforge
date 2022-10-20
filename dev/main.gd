75e452cb6b1656658ffc8691943e048a4848615c
critcap <critcap.dev@gmail.com>
2022-10-20 02:42:17 +0200

    add new scene structure

---
+++ dev/main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+func _ready():
+	var controller = $TestController
+	controller.ui_select = get_node("%TestSelectionMenu")
+	controller.ui_statistics = get_node("%StatisticsMenu")
+	controller.ui_prompt = get_node("%PromptDisplay")
+	
+	controller.start()
