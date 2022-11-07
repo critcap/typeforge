6ac3f61a80f6b16afbd7484e264e4bfbca12435f
William <critcap.dev@gmail.com>
2022-11-07 12:16:29 +0100

    add more commments and more guard clauses

--- typewars/TypingTest/KeyHints.gd
+++ typewars/TypingTest/KeyHints.gd
@@ -1,6 +1,6 @@
 extends HBoxContainer
 
-
+var debug_hints: = {"debug": "debug"}
 var selection_hints: = {"subentry":"", "ui_accept":""}
 
 # region Methods
@@ -11,18 +11,26 @@ func create_hints_from_mapping(hints: Dictionary) -> Array:
 	for key in hints.keys():
 		if !InputMap.has_action(key):
 			continue
-		var binds = ProjectSettings.get_setting(str("input/",key))
-		
+			
+		var binds = ProjectSettings.get_setting(str("input/",key))	
 		if binds.values().empty() || binds.events.empty():
 			continue
+			
+		# get scancode text from bound events
+		# TODO expand filtering for actions with more binds
 		if !(binds.events[0] is InputEventKey):
 			continue
 		var scancode = binds.events[0].get_scancode_with_modifiers()
 		var scancode_string: = OS.get_scancode_string(scancode)
-		var hint_label = Label.new()
-		hint_label.text = scancode_string
-		labels.append(hint_label)
 		
+		# build label and label text
+		var hint_label = Label.new()
+		hint_label.text = str(scancode_string,":",hints[key]).to_lower()
+		labels.append(hint_label)
+	
+	# adds debug options if in debug mode
+	if OS.is_debug_build() && !hints.has("debug"):
+		labels += create_hints_from_mapping(debug_hints)
 	return labels
 	
 	
