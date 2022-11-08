34e6198a554b0dffe0cc1ad51f1e3af600372519
William <critcap.dev@gmail.com>
2022-11-08 10:58:07 +0100

    add shortcut for shader toggle

--- typewars/main.gd
+++ typewars/main.gd
@@ -6,5 +6,11 @@ func _ready():
 	controller.ui_select = get_node("%TestSelectionMenu")
 	controller.ui_statistics = get_node("%StatisticsMenu")
 	controller.ui_prompt = get_node("%InputPrompt")
+	controller.ui_help = $"%InputPromptTutorial"
 
 	controller.start()
+
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("shader_toggle"):
+		$PostProcessing.visible = !$PostProcessing.visible
